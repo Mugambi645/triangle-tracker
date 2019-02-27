52445a16040d70bf83a0773e72b7edc344075af1
mugambi645 <mugambi645@gmail.com>
2019-02-27 15:19:11 +0300

    changes invoked

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,8 @@
-
 function triangletracker() {
   var sideOne =parseInt(document.getElementById('sidea').value);
   var sideTwo =parseInt(document.getElementById('sideb').value);
     var sideThree =parseInt(document.getElementById('sidec').value);
-
+//*!$(document).ready(function(
     if(sideOne + sideTwo >sideThree && sideTwo + sideThree>sideOne && sideOne+sideThree>sideTwo) {
       if(sideOne===sideTwo && sideTwo===sideThree && sideOne===sideThree) {
         alert("This is an equilateral triangle");
