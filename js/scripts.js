6102fdeacc5ad2c92efbc61a2bcfc0ed8dd55bf0
mugambi645 <mugambi645@gmail.com>
2019-02-27 13:46:38 +0300

    capitalization

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+
+function triangletracker() {
+  var sideOne =parseInt(document.getElementById('sidea').value);
+  var sideTwo =parseInt(document.getElementById('sideb').value);
+    var sideThree =parseInt(document.getElementById('sidec').value);
+
+    if(sideOne + sideTwo >sideThree && sideTwo + sideThree>sideOne && sideOne+sideThree>sideTwo) {
+      if(sideOne===sideTwo && sideTwo===sideThree && sideOne===sideThree) {
+        alert("This is an equilateral triangle");
+    }
+else if(sideOne===sideTwo || sidetwo===sideThree || sideOne===sideThree) {
+  alert("This is an isosceles triangle");
+}
+else {
+alert("This is a scalene triangle");
+  }
+}
+else{
+alert("This is not a triangle");
+}
+  }
