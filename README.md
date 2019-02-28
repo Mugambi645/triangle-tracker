066452bd3f894898c9bb35e8ebac6dd886126fc3
mugambi645 <mugambi645@gmail.com>
2019-02-28 11:53:43 +0300

    last changes

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # triangle-tracker
 #function;identifies whether:
-if a triangle has equal sides it is an equilateral
-if a triangle has no equal side it is a scalene
-if it has two equal sides it is an isosceles
+ a triangle has equal sides it is an equilateral
+ a triangle has no equal side it is a scalene
+ it has two equal sides it is an isosceles
   else it is not a triangle at all
--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,17 @@
 function triangletracker() {
-  var sideOne =parseInt(document.getElementById('a').value);
-  var sideTwo =parseInt(document.getElementById('b').value);
-    var sideThree =parseInt(document.getElementById('c').value);
-//*!$(document).ready(function(\
-var sides=[];
-sides.push(sideOne,sideTwo,sideThree);
-if (sides [0] ===sides[1] && sides[1] === sides[2] && sides [0]===sides [2])  {
-  alert("traingle is equivalent");
+  var sideOne = parseInt(document.getElementById('sidea').value);
+  var sideTwo = parseInt(document.getElementById('sideb').value);
+  var sideThree = parseInt(document.getElementById('sidec').value);
 
-    if(sideOne + sideTwo >sideThree && sideTwo + sideThree>sideOne && sideOne+sideThree>sideTwo) {
-      if(sideOne===sideTwo && sideTwo===sideThree && sideOne===sideThree) {
-        alert("This is an equilateral triangle");
+  if (sideOne + sideTwo > sideThree && sideTwo + sideThree > sideOne && sideOne + sideThree > sideTwo) {
+    if (sideOne === sideTwo && sideTwo === sideThree && sideOne === sideThree) {
+      alert("This is an equilateral triangle");
+    } else if (sideOne === sideTwo || sideTwo === sideThree || sideOne === sideThree) {
+      alert("This is an isosceles triangle");
+    } else {
+      alert("This is a scalene triangle");
     }
-else if(sideOne===sideTwo || sideTwo===sideThree || sideOne===sideThree) {
-  alert("This is an isosceles triangle");
-}
-else {
-alert("This is a scalene triangle");
+  } else {
+    alert("This is not a triangle");
   }
 }
-else{
-alert("This is not a triangle");
-}
-  }
